eb25f72a3cb83df83012bc50b56f95561a7c0730
Matheus Aguiar <matheusaguiar.developer@gmail.com>
2023-10-08 22:13:18 -0400

    added script to download media of a profile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tweepy
 import os
+import time
+import random
+import shutil
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,9 +12,10 @@ api_key=os.getenv("API")
 api_key_secret=os.getenv("API_SECRET")
 access_token=os.getenv("ACCESS")
 access_token_secret=os.getenv("ACCESS_SECRET")
+folder_path = "/home/kriza/projetos/twitter-dowloader/twitter-media-downloader-v1.10.1-linux-386/filter/pink-filter/edited"
+destination_folder_path = "kissmeshawtys"
 
 def get_twitter_conn_v1(api_key, api_secret, access_token, access_token_secret) -> tweepy.API:
-    """Get twitter conn 1.1"""
 
     auth = tweepy.OAuth1UserHandler(api_key, api_secret)
     auth.set_access_token(
@@ -21,7 +25,6 @@ def get_twitter_conn_v1(api_key, api_secret, access_token, access_token_secret) 
     return tweepy.API(auth)
 
 def get_twitter_conn_v2(api_key, api_secret, access_token, access_token_secret) -> tweepy.Client:
-    """Get twitter conn 2.0"""
 
     client = tweepy.Client(
         consumer_key=api_key,
@@ -32,13 +35,23 @@ def get_twitter_conn_v2(api_key, api_secret, access_token, access_token_secret) 
 
     return client
 
-def get_media
 
 client_v1 = get_twitter_conn_v1(api_key, api_key_secret, access_token, access_token_secret)
 client_v2 = get_twitter_conn_v2(api_key, api_key_secret, access_token, access_token_secret)
 
-media_path = "myimage.jpg"
-media = client_v1.media_upload(filename=media_path)
-media_id = media.media_id
+while True:
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    if files:
+        random_file = random.choice(files)
+        random_file_path = os.path.join(folder_path, random_file)
+        media = client_v1.media_upload(filename=random_file_path)
+        media_id = media.media_id
+        client_v2.create_tweet(text="", media_ids=[media_id])
+        time.sleep(2)
+        shutil.move(random_file_path, os.path.join(destination_folder_path, random_file))
+        time.sleep(10800)
+    else:
+        print("No files found in the folder.")
 
-client_v2.create_tweet(text="", media_ids=[media_id])
+
+
