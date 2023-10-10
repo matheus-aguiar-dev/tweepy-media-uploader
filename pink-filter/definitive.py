6ec97ecf9db29ab735025e85bf69146d5eaeae40
Matheus Aguiar <matheusaguiar.developer@gmail.com>
2023-10-09 20:11:30 -0400

    change filter

--- pink-filter/definitive.py
+++ pink-filter/definitive.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 
 directory = 'edited'
@@ -10,15 +10,26 @@ for filename in os.listdir(dirImage):
         print(filename)
         img = Image.open(f"{dirImage}/{filename}").convert('RGB')
         width, height = img.size
+
+        # Increase brightness
+
         for x in range(width):
             for y in range(height):
                 r, g, b = img.getpixel((x, y))
+
                 # Apply multipliers to get a pink shade with preserved lightness
                 r = int(r * 1.8)  # Increase red intensity
-                g = int(g * 0.9)  # Decrease green intensity (preserving some lightness)
-                b = int(b * 0.9)  # Decrease blue intensity (preserving some lightness)
+                g = int(g * 0.6)  # Decrease green intensity (preserving some lightness)
+                b = int(b * 0.75)  # Decrease blue intensity (preserving some lightness)
                 img.putpixel((x, y), (r, g, b))
 
-        img.save(f"{directory}{'/'}{count}{'.jpg'}")
-        count = count+1
-# img.save(f"{directory}\{'1.jpg'}")
+
+        enhancer_brightness = ImageEnhance.Brightness(img)
+        img = enhancer_brightness.enhance(1.45)  # Increase brightness (adjust the factor as needed)
+
+        # Increase vibrance
+        enhancer_vibrance = ImageEnhance.Color(img)
+        img = enhancer_vibrance.enhance(1.8)  # Increase vibrance (adjust the factor as needed)
+        img.save(f"{directory}/{count}.jpg")
+        count = count + 1
+
